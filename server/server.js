be67e9b0fec4a3d349fefc2a2c9c5690bfa71155
Rabin Pant <rbnpnt9711@gmail.com>
2026-06-10 10:25:03 +0545

    Updated vercel.json

--- server/server.js
+++ server/server.js
@@ -60,7 +60,6 @@ const io = new Server(httpServer, {
     credentials: true,
   },
 });
-
 // Body parsing
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
